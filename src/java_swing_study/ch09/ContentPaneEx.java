b60c08ef6150f63754e7acdb47e3005026f564e5
lindacoco <lindacoco0204@gmail.com>
2020-01-07 15:20:12 +0900

    예제9-2추가

---
+++ src/java_swing_study/ch09/ContentPaneEx.java
@@ -0,0 +1,42 @@
+package java_swing_study.ch09;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class ContentPaneEx extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ContentPaneEx frame = new ContentPaneEx();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public ContentPaneEx() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
--- src/java_swing_study/ch09/SwingMain.java
+++ src/java_swing_study/ch09/SwingMain.java
@@ -1,9 +1,26 @@
 package java_swing_study.ch09;
 
+import java.awt.EventQueue;
+
+import javax.swing.JPanel;
+
+
+
 public class SwingMain {
 
-	public static void main(String[] args) {
-       MyFrame frame = new MyFrame();
+	    
+		public static void main(String[] args) {
+			EventQueue.invokeLater(new Runnable() {
+				public void run() {
+					try {
+						ContentPaneEx frame = new ContentPaneEx();
+						frame.setVisible(true);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			});
+		}
 	}
 
-}
+
